05471fcd02cb8a180f494fecc7b86273ac620ef3
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:31:26 +0300

    Add files via upload

---
+++ IfElseStatement.java
@@ -0,0 +1,29 @@
+package week_04;
+
+public class IfElseStatement {
+
+    public static void main(String[] args) {
+
+        int num=-12;
+
+        if (num<0){
+            System.out.println(num + " is negative");
+        }
+        else {
+            System.out.println(num + " is not negative");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
